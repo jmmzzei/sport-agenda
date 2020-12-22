f14b9193519259217ec95822a2911bfc8f3af180
José María <josemmazzei@gmail.com>
2020-12-22 20:36:49 -0300

    Split tournament title from events

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 from dotenv import load_dotenv 
 import os
+import re
 
 load_dotenv('.env')
 
@@ -11,7 +12,10 @@ browser.get(os.getenv('BASE_URL'))
 browser.implicitly_wait(1)
 
 for tournament in browser.find_elements_by_css_selector(os.getenv('TOURNAMENT_SELECTOR')):
-  print(tournament.text)
+  events = re.split("^[A-Z\s\W\Ú\Ñ]+", tournament.text)
+  title = re.match("^[A-Z\s\W\Ú\Ñ(1 DE FRANCIA)+]+", tournament.text)
+  print(title.group(0)[:-1])
+  print(events[1])
   print('----------------------------------------')
 
 browser.quit()
