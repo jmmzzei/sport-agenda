b1575c6e0e442446cd3f6a7f99d4e1f3ef4f428a
José María <josemmazzei@gmail.com>
2020-12-22 19:55:49 -0300

    Show all the data

--- server.py
+++ server.py
@@ -9,17 +9,9 @@ load_dotenv('.env')
 browser = webdriver.Firefox()
 browser.get(os.getenv('BASE_URL'))
 browser.implicitly_wait(1)
-titles = []
-i = 0
 
 for tournament in browser.find_elements_by_css_selector(os.getenv('TOURNAMENT_SELECTOR')):
-  titles.append(tournament.text)
-
-for event in browser.find_elements_by_css_selector(os.getenv('EVENT_SELECTOR')):
+  print(tournament.text)
   print('----------------------------------------')
-  print(titles[i])
-  print('----------------------------------------')
-  i = i + 1
-  print(event.text)
 
 browser.quit()
