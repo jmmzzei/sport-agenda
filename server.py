892dbc9b9f5f2829231dee4c06af5502f088e4ff
José María <josemmazzei@gmail.com>
2020-12-23 17:32:06 -0300

    Show the complete agenda
    - The problems were fixed with the addition of nested fors.

--- server.py
+++ server.py
@@ -11,11 +11,22 @@ browser = webdriver.Firefox()
 browser.get(os.getenv('BASE_URL'))
 browser.implicitly_wait(1)
 
-for tournament in browser.find_elements_by_css_selector(os.getenv('TOURNAMENT_SELECTOR')):
-  events = re.split("^[A-Z\s\W\Ú\Ñ]+", tournament.text)
-  title = re.match("^[A-Z\s\W\Ú\Ñ(1 DE FRANCIA)+]+", tournament.text)
-  print(title.group(0)[:-1])
-  print(events[1])
-  print('----------------------------------------')
+for date in browser.find_elements_by_css_selector(os.getenv('DATE')):
+  print('+----------------------------------------+')
+  cDate = date.find_elements_by_css_selector(os.getenv('DATE_TITLE'))[0]
+  print('| ' + cDate.text)
+  print('+----------------------------------------+')
+  for tournament in date.find_elements_by_css_selector(os.getenv('TOURNAMENT')):
+    for title in tournament.find_elements_by_css_selector(os.getenv('TOURNAMENT_TITLE')):
+      print(title.text)
+    for ev in tournament.find_elements_by_css_selector(os.getenv('EVENT')):
+      for events in ev.find_elements_by_css_selector(os.getenv('EVENT_SELECTOR')):
+        hour = events.find_element_by_css_selector(os.getenv('CUSTOM1'))
+        match = events.find_element_by_css_selector(os.getenv('CUSTOM2'))
+        tv = events.find_element_by_css_selector(os.getenv('CUSTOM3'))
+        print(hour.text)
+        print(match.text)
+        print(tv.text)
+  print('+----------------------------------------+')
 
 browser.quit()
